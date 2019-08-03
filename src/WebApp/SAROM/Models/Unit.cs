57e641b29de0e9201fe37d2d40d0d3aabde54f0f
Oliver Appel <olo@apck.de>
2019-08-03 22:03:28 +0200

    Add number of forces to unit

--- src/WebApp/SAROM/Models/Unit.cs
+++ src/WebApp/SAROM/Models/Unit.cs
@@ -4,11 +4,16 @@ namespace SAROM.Models
 {
   public class Unit
   {
+    public int AreaSeeker { get; set; }
+    public int DebrisSearcher { get; set; }
     public string GroupLeader { get; set; }
+    public int Helpers { get; set; }
     public string Id { get; set; }
+    public int Mantrailer { get; set; }
     public string Name { get; set; }
     public string OperationId { get; set; }
     public string PagerNumber { get; set; }
     public List<Person> People { get; set; }
+    public int WaterLocators { get; set; }
   }
 }
