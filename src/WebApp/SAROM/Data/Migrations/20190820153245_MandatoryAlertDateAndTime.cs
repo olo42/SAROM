ff9723159f88a95d55cb87cafb8042adc82dfb68
Oliver Appel <o.appel@topmotive.eu>
2019-08-20 17:37:41 +0200

    Operation Alert date and time should be mandatory #6 !Database drop required if operations without alert date and time exists

---
+++ src/WebApp/SAROM/Data/Migrations/20190820153245_MandatoryAlertDateAndTime.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SAROM.Migrations.Operation
+{
+    public partial class MandatoryAlertDateAndTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AlertTime",
+                table: "Operation",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AlertDate",
+                table: "Operation",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AlertTime",
+                table: "Operation",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AlertDate",
+                table: "Operation",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
