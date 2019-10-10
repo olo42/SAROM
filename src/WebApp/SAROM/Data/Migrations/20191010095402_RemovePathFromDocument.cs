86b2589723a4835da516649960f588aef890ee3f
Oliver Appel <olo@apck.de>
2019-10-10 12:30:57 +0200

    Add ContentType, add Extension, remove path from Document

---
+++ src/WebApp/SAROM/Data/Migrations/20191010095402_RemovePathFromDocument.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SAROM.Migrations.Operation
+{
+    public partial class RemovePathFromDocument : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Path",
+                table: "Document");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContentType",
+                table: "Document",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Extension",
+                table: "Document",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentType",
+                table: "Document");
+
+            migrationBuilder.DropColumn(
+                name: "Extension",
+                table: "Document");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Path",
+                table: "Document",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- src/WebApp/SAROM/Models/Document.cs
+++ src/WebApp/SAROM/Models/Document.cs
@@ -1,18 +1,46 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace SAROM.Models
 {
   public class Document
   {
+    public Document() { }
+    public Document(IFormFile formFile)
+    {
+      Created = DateTime.Now;
+      Name = CreateDocumentName();
+      Extension = "jpg"; // TODO: Get Extension from Content-Type
+      OriginName = formFile.FileName;
+    }
     public DateTime Created { get; set; }
     public string Id { get; set; }
     [Required]
     public string Name { get; set; }
     public string OriginName { get; set; }
     [Required]
-    public string Path { get; set; }
-    [Required]
     public EDocumentType Type { get; set; }
+    public string ContentType { get; set; }
+    public string Extension { get; set; }
+
+    public string FullName
+    {
+      get
+      {
+        return $"{Name}.{Extension}";
+      }
+    }
+
+    private string CreateDocumentName()
+    {
+      return
+          $"{Created.Year}" +
+          $"{Created.Month}" +
+          $"{Created.Day}" +
+          $"{Created.Hour}" +
+          $"{Created.Minute}" +
+          $"{Created.Second}";
+    }
   }
 }
