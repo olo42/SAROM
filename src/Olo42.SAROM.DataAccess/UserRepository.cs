a47d70836bc4dd426ab5607667b09d881e2b9523
Olo42 <olo@apck.de>
2020-02-16 23:27:25 +0100

    Start to work on getting users from repository

--- src/Olo42.SAROM.DataAccess/UserRepository.cs
+++ src/Olo42.SAROM.DataAccess/UserRepository.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Olo42.FileDataAccess.Contracts;
 using Olo42.SAROM.DataAccess.Contracts;
 
@@ -11,22 +13,40 @@ namespace Olo42.SAROM.DataAccess
   {
     private readonly IFileDataAccess<IEnumerable<User>> fileDataAccess;
     private readonly string filePath;
-    private List<User> users;
 
     public UserRepository(IFileDataAccess<IEnumerable<User>> fileDataAccess, string filePath)
     {
       this.fileDataAccess = fileDataAccess;
       this.filePath = filePath;
-      this.users = new List<User>();
     }
 
     public void Add(User user)
     {
-      if (this.users.Contains(user))
+      var users = new List<User>();
+      if (File.Exists(filePath))
+      {
+        users = this.fileDataAccess.Read(filePath).ToList();
+      }
+
+      if (users.Contains(user))
         throw new DuplicateUserException("User already exist!");
 
-      this.users.Add(user);
-      fileDataAccess.Write(filePath, this.users);
+      users.Add(user);
+      fileDataAccess.Write(filePath, users);
+    }
+
+    public IEnumerable<User> Get()
+    {
+      var users = this.fileDataAccess.Read(filePath);
+
+      return users;
+    }
+
+    public User Get(string loginName)
+    {
+      var users = this.fileDataAccess.Read(filePath);
+
+      return users.ToList().Find(x => x.LoginName == loginName);
     }
   }
 }
