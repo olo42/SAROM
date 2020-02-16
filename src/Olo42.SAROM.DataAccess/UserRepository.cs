da3144acb7f5a922f094a4cb7cc3b1c7229747d0
Olo42 <olo@apck.de>
2020-02-16 22:31:39 +0100

    Add user to repository

--- src/Olo42.SAROM.DataAccess/UserRepository.cs
+++ src/Olo42.SAROM.DataAccess/UserRepository.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System.Collections.Generic;
-using System.Text;
+using Olo42.FileDataAccess.Contracts;
+using Olo42.SAROM.DataAccess.Contracts;
 
 namespace Olo42.SAROM.DataAccess
 {
   public class UserRepository
   {
+    private readonly IFileDataAccess<IEnumerable<User>> fileDataAccess;
+    private readonly string filePath;
+    private List<User> users;
+
+    public UserRepository(IFileDataAccess<IEnumerable<User>> fileDataAccess, string filePath)
+    {
+      this.fileDataAccess = fileDataAccess;
+      this.filePath = filePath;
+      this.users = new List<User>();
+    }
+
+    public void Add(User user)
+    {
+      if (this.users.Contains(user))
+        throw new DuplicateUserException("User already exist!");
+
+      this.users.Add(user);
+      fileDataAccess.Write(filePath, this.users);
+    }
   }
 }
---
+++ tests/olo42.SAROM.DataAcces.Tests/UserRepositoryTests/AddUser.cs
@@ -0,0 +1,75 @@
+// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using NUnit.Framework;
+using Olo42.FileDataAccess;
+using Olo42.FileDataAccess.Contracts;
+using Olo42.SAROM.DataAccess;
+using Olo42.SAROM.DataAccess.Contracts;
+
+namespace Olo42.SAROM.DataAcces.Tests.UserRepositoryTests
+{
+  public class AddUser
+  {
+    private const string FILE_PATH = "testusers.dat";
+    private UserRepository userRepository;
+
+    [Test]
+    public void DoNot_let_me_add_two_users_with_same_login_name()
+    {
+      // Arrange
+      var userOne = new User { LoginName = "Peter" };
+      var userTwo = new User { LoginName = "Peter" };
+
+      // Act
+      userRepository.Add(userOne);
+
+      // Assert
+      var ex =
+        Assert.Throws<DuplicateUserException>(() => userRepository.Add(userTwo));
+      Assert.That(ex.Message, Is.EqualTo("User already exist!"));
+    }
+
+    [Test]
+    public void DoNot_let_me_add_user_twice()
+    {
+      // two_users_with_same_login_name
+      // Arrange
+      var user = new User { LoginName = "Peter" };
+
+      // Act
+      userRepository.Add(user);
+
+      // Assert
+      var ex =
+        Assert.Throws<DuplicateUserException>(() => userRepository.Add(user));
+      Assert.That(ex.Message, Is.EqualTo("User already exist!"));
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+      IFileDataAccess<IEnumerable<User>> fileDataAccess =
+        new FormatterDataAccess<IEnumerable<User>>(new BinaryFormatter());
+
+      this.userRepository =
+        new UserRepository(fileDataAccess, FILE_PATH);
+    }
+
+    [Test]
+    public void WritesWhenUserIsAdded()
+    {
+      // Arrange
+      var user = new User();
+
+      // Act
+      this.userRepository.Add(user);
+
+      // Assert
+      Assert.That(File.Exists(FILE_PATH));
+    }
+  }
+}
