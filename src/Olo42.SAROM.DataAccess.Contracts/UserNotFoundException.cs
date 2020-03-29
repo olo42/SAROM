909592d143c6dbd9c505db3723479a8288bea317
Ollo <olo@apck.de>
2020-03-29 23:07:06 +0200

    Create UserNotFoundException.cs

---
+++ src/Olo42.SAROM.DataAccess.Contracts/UserNotFoundException.cs
@@ -0,0 +1,27 @@
+// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Runtime.Serialization;
+
+namespace Olo42.SAROM.DataAccess.Contracts
+{
+  public class UserNotFoundException : Exception
+  {
+    public UserNotFoundException()
+    {
+    }
+
+    public UserNotFoundException(string message) : base(message)
+    {
+    }
+
+    public UserNotFoundException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    protected UserNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+  }
+}
