3d6032fc61a86fbed838b8d310d90a8146f4c1c9
Olo32 <olo@apck.de>
2020-04-12 11:09:40 +0200

    Create OperationFile and Tests

---
+++ tests/Olo42.SAROM.DataAccess.Contract.Tests/OperationFileTests.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NUnit.Framework;
+
+namespace Olo42.SAROM.DataAccess.Contract.Tests
+{
+  [TestFixture]
+  public class OperationFileTests
+  {
+  }
+}
