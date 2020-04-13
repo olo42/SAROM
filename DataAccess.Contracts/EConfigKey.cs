b1df6af149c7c1a1668ff9fb7c17208bb80a18da
Olo32 <olo@apck.de>
2020-04-13 22:33:40 +0200

    Add enums for config section and keys

---
+++ DataAccess.Contracts/EConfigKey.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Olo42.SAROM.DataAccess.Contracts
+{
+  public enum EConfigKey
+  {
+    FileExtension,
+    IndexFile,
+    StoragePath,
+    UsersFile
+  }
+}
