200de6b13b148b8c2d66fa791dbb32145e19bd9a
Olo32 <olo@apck.de>
2020-04-13 15:40:07 +0200

    Create OperationsIndex

---
+++ DataAccess.Contracts/Operations/OperationsIndex.cs
@@ -0,0 +1,28 @@
+// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+
+namespace Olo42.SAROM.DataAccess.Contracts
+{
+  public class OperationsIndex
+  {
+    private readonly List<OperationFile> operationFiles;
+
+    public IEnumerable<OperationFile> OperationFiles
+    {
+      get { return this.operationFiles; }
+    }
+
+    public OperationsIndex()
+    {
+      if (this.OperationFiles == null)
+        this.operationFiles = new List<OperationFile>();
+    }
+
+    public void Add(OperationFile operationFile)
+    {
+      this.operationFiles.Add(operationFile);
+    }
+  }
+}
