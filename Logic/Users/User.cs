829fe0d3c105fdd1b6de98cd4366a12f1cef1a61
Olo32 <olo@apck.de>
2020-05-05 21:59:39 +0200

    Add user

---
+++ Logic/Users/User.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace Olo42.SAROM.Logic.Users
+{
+  [Serializable]
+  public class User
+  {
+    public User()
+    {
+      this.Id = Guid.NewGuid().ToString();
+    }
+
+    public User(string id)
+    {
+      this.Id = id;
+    }
+
+    public string Id { get; private set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string LoginName { get; set; }
+    public string Password { get; set; }
+    public IEnumerable<Role> Roles { get; set; }
+
+    public override bool Equals(object obj)
+    {
+      if (obj == null || GetType() != obj.GetType())
+        return false;
+
+      User otherUser = (User)obj;
+
+      return Id.Equals(otherUser.Id);
+    }
+
+    public override int GetHashCode()
+    {
+      return Id.GetHashCode();
+    }
+  }
+}
