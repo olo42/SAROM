158a3036f8d5818c61874bf250e77c9dad2f3df5
Olo32 <olo@apck.de>
2020-05-05 22:01:55 +0200

    Create interface

---
+++ Logic/Users/IUserManager.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Olo42.SAROM.Logic.Users
+{
+  public interface IUserManager
+  {
+    bool FileExists { get; }
+
+    Task<IEnumerable<User>> Get();
+
+    Task<User> Get(string id);
+
+    Task Store(User user);
+  }
+}
