defc0099ff1a4eb8c5948eb272b579cf8c4626cc
Olo32 <olo@apck.de>
2020-05-05 21:59:11 +0200

    Add role enum

---
+++ Logic/Users/ERoleName.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Olo42.SAROM.Logic.Users
+{
+  public enum ERoleName
+  {
+    Administrator = 1,
+    User = 2
+  }
+}
