ad59e11a76404d9c819863c79d7dc96d1025c68e
Olo32 <olo@apck.de>
2020-05-05 15:32:23 +0200

    Add DisplayFormat for AlertDateTime

--- WebApp/Models/Operation/OperationCreateModel.cs
+++ WebApp/Models/Operation/OperationCreateModel.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System;
 using System.ComponentModel.DataAnnotations;
 
@@ -5,14 +8,16 @@ namespace Olo42.SAROM.WebApp.Models
 {
   public class OperationCreateModel
   {
+    [Required]
     public string Name { get; set; }
 
     [Display(Name = "Einsatznummer")]
+    [Required]
     public string Number { get; set; }
 
     [Display(Name = "Alarmierung")]
     [DataType(DataType.DateTime)]
-    // [DisplayFormat(DataFormatString = "dd.MM.yyyy HH:mm", ApplyFormatInEditMode = true)]
+    [DisplayFormat(DataFormatString = "dd.MM.yyyy HH:mm", ApplyFormatInEditMode = true)]
     [Required]
     public DateTime AlertDateTime { get; set; }
   }
