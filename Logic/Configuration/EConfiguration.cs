99c3e311cd36570a7a7c79bb2caf527d4a9b91b8
Olo32 <olo@apck.de>
2020-05-05 21:58:55 +0200

    Add enum for config entry names

---
+++ Logic/Configuration/EConfiguration.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Olo42.SAROM.Logic.Configuration
+{
+  public enum EConfigKey
+  {
+    FileExtension,
+    IndexFile,
+    StoragePath,
+    UsersFile
+  }
+
+  public enum EConfigSection
+  {
+    SAROMSettings
+  }
+}
