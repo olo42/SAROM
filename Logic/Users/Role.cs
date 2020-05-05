64ccbcb255ede16a010dc85ab93b710fe2e899e5
Olo32 <olo@apck.de>
2020-05-05 21:59:22 +0200

    Add role

---
+++ Logic/Users/Role.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Oliver Appel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Olo42.SAROM.Logic.Users
+{
+  [Serializable]
+  public class Role
+  {
+    public ERoleName Name { get; set; }
+  }
+}
